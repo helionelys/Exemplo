1d0bc0c2fee323b4c06354b64d9839cc0577436d
helionelys <helionelys@gmail.com>
2018-08-14 22:15:55 -0300

    Add files via upload

---
+++ Hello GitHub/src/br/com/helionelys/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.helionelys.git;
+
+/**
+ * @author hbarbosa
+ * Exemplo de integração com o GitHUB
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GITt");
+		System.out.println("Hello Helionelys");
+
+	}
+
+}
